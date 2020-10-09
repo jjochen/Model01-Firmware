0c9347ca897329dac36ab8d06459b58dfcde5c25
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2020-10-09 12:27:15 +0200

    configure atreus keymaps

--- src/Atreus.ino
+++ src/Atreus.ino
@@ -17,47 +17,60 @@
 #include "OneShot.h"
 
 
-
-#define MO(n) ShiftToLayer(n)
-#define TG(n) LockLayer(n)
-
-#define Key_Exclamation LSHIFT(Key_1)
-#define Key_At LSHIFT(Key_2)
-#define Key_Hash LSHIFT(Key_3)
-#define Key_Dollar LSHIFT(Key_4)
-#define Key_Percent LSHIFT(Key_5)
-#define Key_Caret LSHIFT(Key_6)
-#define Key_And LSHIFT(Key_7)
-#define Key_Star LSHIFT(Key_8)
-#define Key_Plus LSHIFT(Key_Equals)
-
 enum {
   QWERTY,
   FUN,
-  UPPER
+  BUTTERFLY
 };
 
 /* *INDENT-OFF* */
 KEYMAPS(
   [QWERTY] = KEYMAP_STACKED
   (
-       Key_Q   ,Key_W        ,Key_E           ,Key_R       ,Key_T
-      ,Key_A   ,Key_S        ,Key_D           ,Key_F       ,Key_G
-      ,Key_Z   ,Key_X        ,Key_C           ,Key_V       ,Key_B, Key_Tab
-      ,Key_Esc ,Key_Backtick ,Key_LeftControl ,Key_LeftGui ,Key_Backspace ,Key_LeftShift
+       Key_Q   ,Key_W            ,Key_E        ,Key_R        ,Key_T
+      ,Key_A   ,Key_S            ,Key_D        ,Key_F        ,Key_G
+      ,Key_Z   ,Key_X            ,Key_C        ,Key_V        ,Key_B         ,Key_Tab
+      ,Key_Esc ,OSM(LeftControl) ,OSM(LeftAlt) ,OSM(LeftGui) ,Key_Backspace ,OSM(LeftShift)
 
-                    ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
-                    ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
-       ,Key_Enter   ,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
-       ,Key_LeftAlt ,Key_Space ,MO(FUN)    ,Key_Minus ,Key_Quote  ,Key_Backslash
+                       ,Key_Y     ,Key_U       ,Key_I      ,Key_O      ,Key_P
+                       ,Key_H     ,Key_J       ,Key_K      ,Key_L      ,Key_Semicolon
+       ,Key_Enter      ,Key_N     ,Key_M       ,Key_Comma  ,Key_Period ,Key_Slash
+       ,OSL(BUTTERFLY) ,Key_Space ,OSL(FUN)    ,LEAD(MAIN) ,Key_Minus  ,Key_Backslash
   ),
 
+  [FUN] = KEYMAP_STACKED
+  (
+       Key_1                ,Key_2 ,Key_3 ,Key_4 ,Key_5
+      ,Key_Backtick         ,___   ,___   ,___   ,___
+      ,LSHIFT(Key_Backtick) ,___   ,___   ,___   ,___        ,___
+      ,___                  ,___   ,___   ,___   ,Key_Delete ,___
+
+           ,Key_6                ,Key_7                 ,Key_8           ,Key_9            ,Key_0
+           ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_LeftBracket ,Key_RightBracket ,Key_Quote
+      ,___ ,___                  ,___                   ,___             ,___              ,Key_Equals
+      ,___ ,___                  ,___                   ,___             ,___              ,___
+   ),
+
+  [BUTTERFLY] = KEYMAP_STACKED
+  (
+      LSHIFT(Key_1) ,LSHIFT(Key_2) ,LSHIFT(Key_3) ,LSHIFT(Key_4) ,LSHIFT(Key_5)
+      ,___          ,___           ,___           ,___           ,___
+      ,___          ,___           ,___           ,___           ,___           ,___
+      ,___          ,___           ,___           ,___           ,Key_Delete    ,___
+
+           ,LSHIFT(Key_6) ,LSHIFT(Key_7) ,LSHIFT(Key_8) ,LSHIFT(Key_9)  ,LSHIFT(Key_0)
+           ,Key_LeftArrow ,Key_DownArrow ,Key_UpArrow   ,Key_RightArrow ,LSHIFT(Key_Quote)
+      ,___ ,___           ,___           ,___           ,___            ,LSHIFT(Key_Equals)
+      ,___ ,___           ,___           ,___           ,___            ,___
+   )
+
+#if ORIGINAL
   [FUN] = KEYMAP_STACKED
   (
        Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
       ,Key_LeftParen   ,Key_LeftArrow    ,Key_DownArrow ,Key_RightArrow       ,Key_RightParen
       ,Key_LeftBracket ,Key_RightBracket ,Key_Hash      ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_Caret
-      ,TG(UPPER)       ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Delete         ,Key_LeftControl
+      ,TG(UPPER)       ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Delete            ,Key_LeftControl
 
                    ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Backspace
                    ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,___
@@ -77,6 +90,7 @@ KEYMAPS(
       ,___      ,XXX           ,Key_F1              ,Key_F2          ,Key_F3         ,Key_F12
       ,___      ,___           ,MoveToLayer(QWERTY) ,Key_PrintScreen ,Key_ScrollLock ,Consumer_PlaySlashPause
    )
+#endif
 )
 /* *INDENT-ON* */
 
