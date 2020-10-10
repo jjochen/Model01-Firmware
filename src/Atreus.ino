21acbc2ce8d6292bf031b11dc33166f95237638b
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2020-10-10 23:44:06 +0200

    add EDIT and XCODE layers

--- src/Atreus.ino
+++ src/Atreus.ino
@@ -16,11 +16,15 @@
 #include "Leader.h"
 #include "OneShot.h"
 
+#include "keydefs.h"
+
 
 enum {
   QWERTY,
   FUN,
-  BUTTERFLY
+  UPPER,
+  EDIT,
+  XCODE,
 };
 
 /* *INDENT-OFF* */
@@ -32,18 +36,18 @@ KEYMAPS(
       ,Key_Z   ,Key_X            ,Key_C        ,Key_V        ,Key_B         ,Key_Tab
       ,Key_Esc ,OSM(LeftControl) ,OSM(LeftAlt) ,OSM(LeftGui) ,Key_Backspace ,OSM(LeftShift)
 
-                       ,Key_Y     ,Key_U       ,Key_I      ,Key_O      ,Key_P
-                       ,Key_H     ,Key_J       ,Key_K      ,Key_L      ,Key_Semicolon
-       ,Key_Enter      ,Key_N     ,Key_M       ,Key_Comma  ,Key_Period ,Key_Slash
-       ,OSL(BUTTERFLY) ,Key_Space ,OSL(FUN)    ,LEAD(MAIN) ,Key_Minus  ,Key_Backslash
+                   ,Key_Y     ,Key_U       ,Key_I      ,Key_O      ,Key_P
+                   ,Key_H     ,Key_J       ,Key_K      ,Key_L      ,Key_Semicolon
+       ,Key_Enter  ,Key_N     ,Key_M       ,Key_Comma  ,Key_Period ,Key_Slash
+       ,OSL(UPPER) ,Key_Space ,OSL(FUN)    ,LEAD(MAIN) ,Key_Minus  ,Key_Backslash
   ),
 
   [FUN] = KEYMAP_STACKED
   (
        Key_1        ,Key_2 ,Key_3 ,Key_4 ,Key_5
-      ,Key_Backtick ,___   ,___   ,___   ,___
-      ,___          ,___   ,___   ,___   ,___        ,___
-      ,___          ,___   ,___   ,___   ,Key_Delete ,___
+      ,Key_Backtick ,___   ,___   ,___   ,Key_PageUp
+      ,___          ,___   ,___   ,___   ,Key_PageDown ,___
+      ,___          ,___   ,___   ,___   ,Key_Delete   ,___
 
            ,Key_6         ,Key_7         ,Key_8       ,Key_9          ,Key_0
            ,Key_LeftArrow ,Key_DownArrow ,Key_UpArrow ,Key_RightArrow ,Key_Quote
@@ -51,20 +55,62 @@ KEYMAPS(
       ,___ ,___           ,___           ,___         ,___            ,___
    ),
 
-  [BUTTERFLY] = KEYMAP_STACKED
+  [UPPER] = KEYMAP_STACKED
   (
        LSHIFT(Key_1)        ,LSHIFT(Key_2) ,LSHIFT(Key_3) ,LSHIFT(Key_4) ,LSHIFT(Key_5)
       ,LSHIFT(Key_Backtick) ,___           ,___           ,___           ,___
       ,___                  ,___           ,___           ,___           ,___           ,___
-      ,___                  ,___           ,___           ,___           ,Key_Delete    ,___
+      ,LockLayer(EDIT)      ,___           ,___           ,___           ,Key_Delete    ,___
 
            ,LSHIFT(Key_6)        ,LSHIFT(Key_7)         ,LSHIFT(Key_8)   ,LSHIFT(Key_9)    ,LSHIFT(Key_0)
            ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_LeftBracket ,Key_RightBracket ,LSHIFT(Key_Quote)
       ,___ ,___                  ,___                   ,___             ,___              ,LSHIFT(Key_Equals)
       ,___ ,___                  ,___                   ,___             ,___              ,___
-   )
+   ),
+
+  [EDIT] = KEYMAP_STACKED
+  (
+       Key_Insert          ,Key_Home      ,Key_UpArrow   ,Key_End        ,Key_Tab
+      ,___                 ,Key_LeftArrow ,Key_DownArrow ,Key_RightArrow ,___
+      ,Key_Z               ,Key_X         ,Key_C         ,Key_V          ,Key_Space     ,Key_Enter
+      ,MoveToLayer(QWERTY) ,___           ,___           ,___            ,Key_Backspace ,___
+
+           ,___ ,___ ,___ ,___ ,___
+           ,___ ,___ ,___ ,___ ,___
+      ,___ ,___ ,___ ,___ ,___ ,___
+      ,___ ,___ ,___ ,___ ,___ ,___
+   ),
+
+  [XCODE] = KEYMAP_STACKED
+  (
+       JJ_XcodeCloseOtherEditors ,JJ_XcodeCloseEditor ,JJ_XcodeEditAllInScope   ,JJ_XcodeFindAndReplaceInWorkspace ,JJ_XcodeNewEditor
+      ,JJ_XcodeShowCodeActions   ,JJ_XcodeSelectLine  ,JJ_XcodeJumpToDefinition ,JJ_XcodeFindInWorkspace           ,JJ_XcodeFindNextInWorkspace
+      ,XXX                       ,XXX                 ,JJ_XcodeActivateConsole  ,JJ_XcodeRefactorExtractToVariable ,JJ_XcodeNewEditorBelow      ,___
+      ,___                       ,___                 ,___                      ,___                               ,___                         ,___
+
+                           ,JJ_XcodeRevealInProjectNavigator ,JJ_XcodeRevealInDebugNavigator            ,XXX                           ,JJ_XcodeOpenQuickly  ,JJ_XcodeUppercaseWord
+                           ,JJ_XcodeGoBack                   ,JJ_XcodeJumpToNextCounterpartInNextEditor ,JJ_XcodeJumpToNextCounterpart ,JJ_XcodeGoForward    ,JJ_XcodeCapitalizeWord
+      ,JJ_XcodeAssistant   ,JJ_XcodeRefactorRename           ,JJ_XcodeRefactorExtractToMethod           ,JJ_XcodeMoveLineUp            ,JJ_XcodeMoveLineDown ,JJ_XcodeLowercaseWord
+      ,JJ_XcodeFocusEditor ,___                              ,___                                       ,___                           ,___                  ,JJ_XcodeJumpToNextChange
+   ),
 
 #if ORIGINAL
+  
+  [XCODE] =  KEYMAP_STACKED
+  (XXX,            JJ_XcodeShowRelatedItems,  JJ_XcodeCopySymbolName, JJ_XcodeCopyQualifiedSymbolName, JJ_XcodeFindCallHierarchy,         JJ_XcodeFindSelectedSymbolInWorkspace, JJ_XcodeMoveFocusToNextArea,
+   XXX,            JJ_XcodeCloseOtherEditors, JJ_XcodeCloseEditor,    JJ_XcodeEditAllInScope,          JJ_XcodeFindAndReplaceInWorkspace, JJ_XcodeNewEditor,                     JJ_XcodeMoveFocusToNextEditor,
+   JJ_XcodeCommit, JJ_XcodeShowCodeActions,   JJ_XcodeSelectLine,     JJ_XcodeJumpToDefinition,        JJ_XcodeFindInWorkspace,           JJ_XcodeFindNextInWorkspace,
+   JJ_XcodePull,   XXX,                       XXX,                    JJ_XcodeActivateConsole,         JJ_XcodeRefactorExtractToVariable, JJ_XcodeNewEditorBelow,                XXX,
+   ___, ___, ___, ___,
+   ___,
+
+   ___,                 JJ_XcodeRevealInProjectNavigator, JJ_XcodeRevealInDebugNavigator,            XXX,                           XXX,                  JJ_XcodeShowLibrary,      JJ_XcodeUppercaseWord,
+   JJ_XcodeAssistant,   JJ_XcodePauseContinue,            JJ_XcodeContinueToCurrentLine,             XXX,                           JJ_XcodeOpenQuickly,  JJ_XcodeShowSpelling,     JJ_XcodeCapitalizeWord,
+                        JJ_XcodeGoBack,                   JJ_XcodeJumpToNextCounterpartInNextEditor, JJ_XcodeJumpToNextCounterpart, JJ_XcodeGoForward,    JJ_XcodeCheckSpelling,    JJ_XcodeLowercaseWord,
+   JJ_XcodeFocusEditor, JJ_XcodeRefactorRename,           JJ_XcodeRefactorExtractToMethod,           JJ_XcodeMoveLineUp,            JJ_XcodeMoveLineDown, JJ_XcodeCommentSelection, JJ_XcodeJumpToNextChange,
+   ___, ___, ___, ___,
+   ___),
+
   [FUN] = KEYMAP_STACKED
   (
        Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
